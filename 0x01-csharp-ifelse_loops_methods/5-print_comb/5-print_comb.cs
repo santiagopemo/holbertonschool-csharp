b22058bcf4bbafd1c7d4bd8ae895b3d7d5771e88
santiagopemo <santiagopemo@gmail.com>
2020-12-01 16:15:53 -0500

    added tasks 5 and 6

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 99; i++)
+            {
+                string output = i.ToString("D2");
+                if (i < 99)
+                    output += ", ";
+                else
+                    output += "\n";
+                Console.Write(output);
+            }
+        }
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 9; i++)
+            {
+                for(int j = 0; j <= 9; j++)
+                {
+                    if (i == j || j < i)
+                        continue;
+                    if (i == 8 && j == 9)
+                        Console.WriteLine($"{i}{j}");
+                    else
+                        Console.Write($"{i}{j}, ");                                            
+                }
+            };
+        }
+    }
+}
