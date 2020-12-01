22343bc9e992a8d096a6dd3acdff83169d9cc48f
santiagopemo <santiagopemo@gmail.com>
2020-12-01 17:34:34 -0500

    added tasks 7, 8 and 9

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Character
+{       
+    public static bool IsLower(char c)
+    {
+        if ( c <= 'z' && c >= 'a')
+            return true;
+        else
+            return false;
+    }
+}
