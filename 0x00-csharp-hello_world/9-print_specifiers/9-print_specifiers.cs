319ec8de5ce08cc5071bad871b8040bee3d7509d
santiagopemo <santiagopemo@gmail.com>
2020-11-30 21:27:43 -0500

    added task 9

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine($"Percent: {percent:0.00%}");
+        Console.WriteLine($"Currency: {currency:C}");
+	}
+}
