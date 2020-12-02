35420af2a288f037e38142d2c5cc57a4848db1f3
santiagopemo <santiagopemo@gmail.com>
2020-12-02 15:02:55 -0500

    added tasks 8 and 9

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int keysCount = 0;
+        
+        foreach (KeyValuePair<string, string> kvp in myDict)
+        {
+            keysCount++;
+        }
+        return keysCount;
+    }
+}
