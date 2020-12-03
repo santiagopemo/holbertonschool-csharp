d6f132782144b8bdf225170cf84ea52b268a3d8d
santiagopemo <santiagopemo@gmail.com>
2020-12-02 19:35:21 -0500

    added tasks 12 and 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        SortedList<string, string> newDict = new SortedList<string, string>(myDict);
+        
+        foreach (KeyValuePair<string, string> kvp in newDict)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+            return "None";
+        int i = 0;
+        KeyValuePair<string, int> bestScore = new KeyValuePair<string, int>();
+
+        foreach (KeyValuePair<string, int> kvp in myList)
+        {
+            if (i == 0 || kvp.Value > bestScore.Value)
+                bestScore = kvp;
+            i++;
+        }
+        return bestScore.Key;
+    }
+}
