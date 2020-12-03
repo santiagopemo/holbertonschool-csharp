5286b6763d1ed9044e40a5d749c027797b1608b5
santiagopemo <santiagopemo@gmail.com>
2020-12-02 20:23:51 -0500

    added task 14, 15 and 100

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[][]
+            {
+                new int[] {0, 1, 2, 3},
+                new int[] {0, 1, 2, 3, 4, 5, 6},
+                new int[] {0, 1}
+            };
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j]);
+                    if (j < jaggedArray[i].Length - 1)
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2D = new int[5, 5];
+
+            array2D[2, 2] = 1;
+            for (int i = 0; i < array2D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2D.GetLength(1); j++)
+                {
+                    Console.Write(array2D[i, j]);
+                    if (j < array2D.GetLength(1) - 1)
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
