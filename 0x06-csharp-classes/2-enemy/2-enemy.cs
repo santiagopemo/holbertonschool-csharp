997e62d8860be00a161abcc771cdb14985ec1abc
santiagopemo <santiagopemo@gmail.com>
2020-12-15 16:17:10 -0500

    added task 2

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Represents a Zombie Enemy.</summary>
+    class Zombie
+    {
+        /// <summary>Public field health.</summary>
+        public int health;
+
+        /// <summary>Initializes a new instance of the <see cref="Enemies.Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>Initializes a new instance of the <see cref="Enemies.Zombie"/> class.</summary>
+        /// <param name="value">Zombies's health value.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+    }
+}
