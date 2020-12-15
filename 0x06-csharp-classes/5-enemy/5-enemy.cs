ce657839eca140e50edbc429182996cb4a790890
santiagopemo <santiagopemo@gmail.com>
2020-12-15 17:55:33 -0500

    added task 5

--- 0x06-csharp-classes/5-enemy/5-enemy.cs
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -13,7 +13,7 @@ namespace Enemies
         /// <summary>Initializes a new instance of the <see cref="Enemies.Zombie"/> class.</summary>
         public Zombie()
         {
-            health = 0;
+            this.health = 0;
         }
         /// <summary>Initializes a new instance of the <see cref="Enemies.Zombie"/> class.</summary>
         /// <param name="value">Zombies's health value.</param>
@@ -21,22 +21,22 @@ namespace Enemies
         {
             if (value < 0)
                 throw new ArgumentException("Health must be greater than or equal to 0");
-            health = value;
+            this.health = value;
         }
         /// <summary>Returns the value of health a the Zombie object.</summary>
-        public int GetHealth() => health; 
+        public int GetHealth() => this.health; 
          
         /// <summary>Gets or sets the name.</summary>
         public string Name
         {
-            get => name;
-            set => name = value;
+            get => this.name;
+            set => this.name = value;
         }
 
          /// <summary>Prints the Zombie object’s attributes to stdout</summary>
         public override string ToString()
         {
-            return string.Format("Zombie name: {0} / Total Health: {1}", name, health);
+            return string.Format("Zombie name: {0} / Total Health: {1}", this.name, this.health);
         }
     }
 }
