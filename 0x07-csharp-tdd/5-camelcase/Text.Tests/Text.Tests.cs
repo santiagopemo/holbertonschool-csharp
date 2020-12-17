4e1b249ca666c8ba0a2b044d18a10080e04eb197
santiagopemo <santiagopemo@gmail.com>
2020-12-17 16:11:05 -0500

    added task 5

---
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    public class Text_Str_Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void String_CamelCase()
+        {
+            string s = "camelCaseTestOne";
+            int result = Str.CamelCase(s);
+            Assert.AreEqual(4, result);
+        }
+        [Test]
+        public void String_Empty()
+        {
+            string s = "";
+            int result = Str.CamelCase(s);
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void String_Lower()
+        {
+            string s = "helloworld";
+            int result = Str.CamelCase(s);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>Represents Text Operations.</summary>
+    public class Str
+    {
+        /// <summary>Method that determines how many words are in a string.</summary>
+        /// <param name="s">A string.</param>
+        /// <returns>number of words in s</returns>
+        public static int CamelCase(string s)
+        {
+            int count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (count == 0)
+                    count++;
+                if ('A' <= s[i] && s[i] <= 'Z')
+                    count++;
+            }
+            return count;
+        }
+    }
+}
