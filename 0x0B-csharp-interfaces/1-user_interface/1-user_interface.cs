2f4e6cfda1eebe9177e3e4ea93212f7a1cefdd58
santiagopemo <santiagopemo@gmail.com>
2021-03-23 16:22:46 -0500

    corrected  1

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,59 +1,59 @@
 ﻿using System;
 
-    ///<summary> Base class </summary>
-    public abstract class Base
-    {
-     ///<summary> name property </summary>
+///<summary> Base class </summary>
+public abstract class Base
+{
+    ///<summary> name property </summary>
     private string name {get; set;}
-     ///<summary> String representation of the object </summary>
+        ///<summary> String representation of the object </summary>
     public override string ToString()
     {
         return $"{name} is a {this.GetType()}";
     }
-    }
+}
 
 
-     ///<summary> IInteractive </summary>
-    public interface IInteractive {
-         ///<summary> overrider </summary>
-        void Interact();
-    }
-     ///<summary> overrider </summary>
-    public interface IBreakable{
-         ///<summary> overrider </summary>
-         /// <value>Getter and setter</value>
-         int durability {get; set;}
-          ///<summary> overrider </summary>
-         void Break();
-    }
-     ///<summary> overrider </summary>
-    public interface ICollectable{
-         ///<summary> overrider </summary>
-         /// <value>Getter and setter</value>
-        bool isCollected {get; set;}
-         ///<summary> overrider </summary>
-        void Collect();
-    }
-     ///<summary> overrider </summary>
-    public class TestObject : Base, IInteractive, IBreakable, ICollectable{
-         ///<summary> overrider </summary>
-         /// <value>Getter and setter</value>
-        public int durability {get; set;}
+    ///<summary> IInteractive </summary>
+public interface IInteractive {
+        ///<summary> overrider </summary>
+    void Interact();
+}
+    ///<summary> overrider </summary>
+public interface IBreakable{
         ///<summary> overrider </summary>
         /// <value>Getter and setter</value>
-        public bool isCollected {get; set;}
-        ///<summary>  override </summary>
-        public string name {get; set;}
+        int durability {get; set;}
         ///<summary> overrider </summary>
-        public void Interact(){
-            // Method implementation.
-        } 
-         ///<summary> overrider </summary>
-        public void Break(){
-            // Method implementation.
-        }   
-         ///<summary> overrider </summary>
-        public void Collect(){
-            // Method implementation.
-        }
+        void Break();
+}
+    ///<summary> overrider </summary>
+public interface ICollectable{
+        ///<summary> overrider </summary>
+        /// <value>Getter and setter</value>
+    bool isCollected {get; set;}
+        ///<summary> overrider </summary>
+    void Collect();
+}
+    ///<summary> overrider </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable{
+        ///<summary> overrider </summary>
+        /// <value>Getter and setter</value>
+    public int durability {get; set;}
+    ///<summary> overrider </summary>
+    /// <value>Getter and setter</value>
+    public bool isCollected {get; set;}
+    ///<summary>  override </summary>
+    public string name {get; set;}
+    ///<summary> overrider </summary>
+    public void Interact(){
+        // Method implementation.
+    } 
+        ///<summary> overrider </summary>
+    public void Break(){
+        // Method implementation.
+    }   
+        ///<summary> overrider </summary>
+    public void Collect(){
+        // Method implementation.
     }
+}
