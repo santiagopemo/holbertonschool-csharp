b7c92ef6c7aaf472fc5172a5409e995480379c9c
santiagopemo <santiagopemo@gmail.com>
2021-03-23 15:43:55 -0500

    added task 6

--- 0x0B-csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
+++ 0x0B-csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
@@ -1,12 +1,162 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace _6_generic_iteration
+///<summary>Base Class</summary>
+abstract class Base
 {
-    class Program
+    ///<summary>Property name</summary>
+    public string name { set; get; }
+
+    ///<summary>Returns the string representation of a Base object</summary>
+    public override string ToString()
     {
-        static void Main(string[] args)
+        return $"{this.name} is a {this.GetType().Name}";
+    }
+}
+
+///<summary>IInteractive Interface</summary>
+interface IInteractive
+{
+    ///<summary>Delcaration of method Interact</summary>
+
+    void Interact();
+}
+
+///<summary>IBreakable Interface</summary>
+interface IBreakable
+{
+    ///<summary>Delcaration of property durability</summary>
+    int durability { set; get; }
+
+    ///<summary>Delcaration of method Break</summary>
+    void Break();
+}
+
+///<summary>ICollectable Interface</summary>
+interface ICollectable
+{
+    ///<summary>Delcaration of property isCollected</summary>
+    bool isCollected { set; get; }
+
+    ///<summary>Delcaration of method Collect</summary>
+    void Collect();
+}
+
+
+
+///<summary>Door Class</summary>
+class Door : Base, IInteractive
+{
+    ///<summary>Base Constructor</summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+    ///<summary>Method Interact</summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {this.name}. It's locked.");
+    }
+}
+
+class Decoration : Base, IInteractive, IBreakable 
+{
+    public bool isQuestItem;
+
+    public int durability { get; set; }
+
+    public Decoration (string name = "Decoration", int durability = 1, bool isQuestItem = false)
+    {
+        if (durability <= 0)
         {
-            Console.WriteLine("Hello World!");
+            throw new Exception("Durability must be greater than 0");
+        }
+        this.name = name;
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
+    }
+
+    public void Interact()
+    {
+        if (durability <= 0)
+        {
+            Console.WriteLine($"The {this.name} has been broken.");
+        }
+        else if (isQuestItem)
+        {
+            Console.WriteLine($"You look at the {this.name}. There's a key inside.");
+        }
+        else if (isQuestItem == false)
+        {
+            Console.WriteLine($"You look at the {this.name}. Not much to see here.");
         }
     }
+
+    public void Break()
+    {
+        durability--;
+        if (durability > 0)
+        {
+            Console.WriteLine($"You hit the {this.name}. It cracks.");
+        }
+        if (durability == 0)
+        {
+            Console.WriteLine($"You smash the {this.name}. What a mess.");
+        }
+        if (durability < 0)
+        {
+            Console.WriteLine($"The {this.name} is already broken.");
+        }
+    }
+}
+
+class Key : Base, ICollectable
+{
+    public bool isCollected { get; set; }
+
+    public Key(string name = "Key", bool isCollected = false)
+    {
+        this.name = name;
+        this.isCollected = isCollected;
+    }
+
+    public void Collect()
+    {
+        if (isCollected)
+        {
+            Console.WriteLine($"You have already picked up the {this.name}.");
+        }
+        else
+        {
+            isCollected = true;
+            Console.WriteLine($"You pick up the {this.name}.");
+        }
+    }
+}
+
+class Objs<T> : IEnumerable<T>
+{    
+    List<T> objects = new List<T>();
+
+    public T this[int index]
+    {
+        get { return objects[index]; }  
+        set { objects.Insert(index, value); }  
+    }
+
+    public void Add(T data)
+    {
+        objects.Add(data);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return objects.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }   
 }
