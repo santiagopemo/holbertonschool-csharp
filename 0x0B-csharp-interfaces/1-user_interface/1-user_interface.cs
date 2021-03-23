3136385e1af1fede04e406e4ce2bc87af684f027
santiagopemo <santiagopemo@gmail.com>
2021-03-23 16:17:30 -0500

    corrected  1

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,74 +1,40 @@
 ﻿using System;
 
-///<summary>Base Class</summary>
-public abstract class Base
-{
-    ///<summary>Property name</summary>
-    public string name { get; set; }
-
-    ///<summary>Returns the string representation of a Base object</summary>
+    ///<summary> Base class </summary>
+    public abstract class Base
+    {
+    
+    private string name {get; set;}
+    
     public override string ToString()
     {
-        return $"{name} is a {GetType()}";
+        return $"{name} is a {this.GetType()}";
     }
-}
-
-///<summary>IInteractive Interface</summary>
-public interface IInteractive
-{
-    ///<summary>Delcaration of method Interact</summary>
-
-    void Interact();
-}
-
-///<summary>IBreakable Interface</summary>
-public interface IBreakable
-{
-    ///<summary>Delcaration of property durability</summary>
-    int durability { get; set; }
-
-    ///<summary>Delcaration of method Break</summary>
-    void Break();
-}
-
-///<summary>ICollectable Interface</summary>
-public interface ICollectable
-{
-    ///<summary>Delcaration of property isCollected</summary>
-    bool isCollected { get; set; }
-
-    ///<summary>Delcaration of method Collect</summary>
-    void Collect();
-}
-
-///<summary>TestObject Class</summary>
-public class TestObject : Base, IInteractive, IBreakable, ICollectable
-{
-    ///<summary>Property durability</summary>
-    public int durability { get; set;}
-
-    ///<summary>Property isCollected</summary>
-    public bool isCollected { get; set; }
-
-    ///<summary>Property name</summary>
-
-    public string name { get; set; }
-
-    ///<summary>Method Interact with default implementation</summary>
-    public void Interact()
-    {
-        throw new NotImplementedException();
     }
 
-    ///<summary>Method Break with default implementation</summary>
-    public void Break()
-    {
-        throw new NotImplementedException();
-    }
 
-    ///<summary>Method Collect with default implementation</summary>
-    public void Collect()
-    {
-        throw new NotImplementedException();
+    
+    public interface IInteractive {
+         
+        void Interact();
     }
-}
+     
+    public interface IBreakable{
+        int durability {get; set;}
+         void Break();
+    }
+    public interface ICollectable{
+        bool isCollected {get; set;}
+        void Collect();
+    }
+    public class TestObject : Base, IInteractive, IBreakable, ICollectable{
+        public int durability {get; set;}
+        public bool isCollected {get; set;}
+        public string name {get; set;}
+        public void Interact(){
+        } 
+        public void Break(){
+        }   
+        public void Collect(){
+        }
+    }
