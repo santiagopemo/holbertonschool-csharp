790fa93d84d4f8d2fe35cdd825aea3bac04d0dd5
santiagopemo <santiagopemo@gmail.com>
2021-03-23 16:20:02 -0500

    corrected  1

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -3,9 +3,9 @@
     ///<summary> Base class </summary>
     public abstract class Base
     {
-    
+     ///<summary> name property </summary>
     private string name {get; set;}
-    
+     ///<summary> String representation of the object </summary>
     public override string ToString()
     {
         return $"{name} is a {this.GetType()}";
@@ -13,28 +13,46 @@
     }
 
 
-    
+     ///<summary> IInteractive </summary>
     public interface IInteractive {
-         
+         ///<summary> overrider </summary>
         void Interact();
     }
-     
+     ///<summary> overrider </summary>
     public interface IBreakable{
-        int durability {get; set;}
+         ///<summary> overrider </summary>
+         /// <value>Getter and setter</value>
+         int durability {get; set;}
+          ///<summary> overrider </summary>
          void Break();
     }
+     ///<summary> overrider </summary>
     public interface ICollectable{
+         ///<summary> overrider </summary>
+         /// <value>Getter and setter</value>
         bool isCollected {get; set;}
+         ///<summary> overrider </summary>
         void Collect();
     }
+     ///<summary> overrider </summary>
     public class TestObject : Base, IInteractive, IBreakable, ICollectable{
+         ///<summary> overrider </summary>
+         /// <value>Getter and setter</value>
         public int durability {get; set;}
+        ///<summary> overrider </summary>
+        /// <value>Getter and setter</value>
         public bool isCollected {get; set;}
-        public string name {get; set;}
+        
+        ///<summary> overrider </summary>
         public void Interact(){
+            // Method implementation.
         } 
+         ///<summary> overrider </summary>
         public void Break(){
+            // Method implementation.
         }   
+         ///<summary> overrider </summary>
         public void Collect(){
+            // Method implementation.
         }
     }
